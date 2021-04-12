e44dcd8f9e422f5d8849b18e745c42ef13244fca
Hanna-alex <ipantevaanna@gmail.com>
2021-04-13 02:02:02 +0600

    create: js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+  let menuButton = document.querySelector('.menu-button');
+  let menu = document.querySelector('.navbar-mobile');
+
+  menuButton.addEventListener('click', function(){
+    console.log('click'),
+    menuButton.classList.toggle('menu-button__active'),
+    menu.classList.toggle('navbar-mobile__active')
+});
+
+
